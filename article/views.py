419312081abcb5dd762fc240b6ba00f644a398da
黄赟 <huangyun@hy.local>
2023-08-22 22:06:02 +0800

    文章API完成

--- article/views.py
+++ article/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from article.models import Article
+
+from article.serializers import ArticleListSerializer
 
 # Create your views here.
+
+def article_list(request):
+    articles = Article.objects.all()   #取出所有文章 QuerySet
+    serializer = ArticleListSerializer(articles, many=True) # 创建序列化容器
+    return JsonResponse(serializer.data, safe=False)  # 返回JSON格式数据
+
